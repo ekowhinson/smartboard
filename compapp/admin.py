343a9f40cd02f5da1b5cd8f3529ccf7a87e810cf
Francis Ekow Hinson <ekowhinson@gmail.com>
2021-11-17 16:58:33 +0000

    moduleadmin

--- compapp/admin.py
+++ compapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from compapp.models import CiAdmin, CiAdminRoles
+from compapp.models import CiAdmin, CiAdminRoles, Module
 
 # Register your models here.
 admin.site.register(CiAdmin)
 admin.site.register(CiAdminRoles)
+admin.site.register(Module)
