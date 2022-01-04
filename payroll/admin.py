7ea0c25912dc149867c3e538acacd68b2f5ded17
Francis Ekow Hinson <ekowhinson@gmail.com>
2022-01-04 19:01:44 +0000

    payroll tables

--- payroll/admin.py
+++ payroll/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Affordability, Employee,ActivityLog,LoginAttempts,Company,CompanyBranch, Mandate, Payment,Bank,BankBranch,Element,ElementGroup,ElementCategory,TesterTable
+from .models import Affordability, AuthorityNote, CompanyRate, CompanySettings, Employee,ActivityLog,LoginAttempts,Company,CompanyBranch, Mandate, Payment,Bank,BankBranch,Element,ElementGroup,ElementCategory, Positions, Product, Rejections,TesterTable, Transaction, UserElement
 # Register your models here.
 
 admin.site.register(Employee)
@@ -16,3 +16,14 @@ admin.site.register(Element)
 admin.site.register(ElementCategory)
 admin.site.register(ElementGroup)
 admin.site.register(TesterTable)
+admin.site.register(AuthorityNote)
+admin.site.register(Bank)
+admin.site.register(BankBranch)
+admin.site.register(CompanyRate)
+admin.site.register(CompanySettings)
+admin.site.register(Positions)
+admin.site.regiser(Product)
+admin.site.register(Rejections)
+admin.site.register(Transaction)
+admin.site.register(UserElement)
+
