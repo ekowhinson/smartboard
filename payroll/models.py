6f10301d49f8d9ed04ae299a074283d7e788e775
Francis Ekow Hinson <ekowhinson@gmail.com>
2022-01-04 19:07:29 +0000

    tester

--- payroll/models.py
+++ payroll/models.py
@@ -288,3 +288,4 @@ class UserElement(models.Model):
 class TesterTable(models.Model):
     code=models.CharField(max_length=50)
     name=models.CharField(max_length=100)
+    age=models.DecimalField(max_digits=8,decimal_places=2)
