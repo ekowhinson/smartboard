8df0c04b8eb804f77833b1c819bc8c637d8b76e4
Francis Ekow Hinson <ekowhinson@gmail.com>
2022-01-17 14:32:25 +0000

    mandate code filter

--- payroll/views.py
+++ payroll/views.py
@@ -60,8 +60,8 @@ class PaymentViewSet(viewsets.ModelViewSet):
 class MandateViewSet(viewsets.ModelViewSet):
     queryset=Mandate.objects.all()
     serializer_class=MandateSerializer
-    filterset_fields=['employee_code']
-    
+    filterset_fields=['employee_code','code']
+
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset= Employee.objects.all()
     serializer_class=EmployeeSerializer
