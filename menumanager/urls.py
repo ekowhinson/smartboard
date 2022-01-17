f94bcf81257e1fea56d7a86798246262adb07852
Francis Ekow Hinson <ekowhinson@gmail.com>
2022-01-17 14:54:19 +0000

    eventtype url

--- menumanager/urls.py
+++ menumanager/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
-from .views import MenuUserElementViewSet,MenuSubUsersDelete,MenuGroupViewSet,MenuSubGroupDetailViewSet,MenuSubGroupViewSet,MenuSubUsersViewSet,DaUsersViewSet,UsersPermissionViewSet
+from .views import EventLogViewSet, EventTypeViewSet, MenuUserElementViewSet,MenuSubUsersDelete,MenuGroupViewSet,MenuSubGroupDetailViewSet,MenuSubGroupViewSet,MenuSubUsersViewSet,DaUsersViewSet,UsersPermissionViewSet
 
 router=SimpleRouter()
 router.register('menusubusersdelete',MenuSubUsersDelete,basename='menusubusersdelete')
@@ -11,5 +11,6 @@ router.register('menusubusers',MenuSubUsersViewSet,basename='menusubusers')
 router.register('dausers',DaUsersViewSet,basename='dausers')
 router.register('userpermission',UsersPermissionViewSet,'userpermission')
 router.register('menuuserelement',MenuUserElementViewSet,basename='menuuserelement')
-
+router.register('eventlog',EventLogViewSet,basename='eventlog')
+router.register('eventtype',EventTypeViewSet,basename='eventtype')
 urlpatterns=router.urls
