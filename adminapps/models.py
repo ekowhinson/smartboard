cc3958afa4d01f6f482581961c920c6ce8e5af12
Francis Ekow Hinson <ekowhinson@gmail.com>
2022-01-18 11:20:20 +0000

    cal window

--- adminapps/models.py
+++ adminapps/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Calendar(models.Model):
+    month=models.CharField(max_length=30)
+    Year=models.IntegerField()
+    period=models.CharField(max_length=80)
+    startdate=models.DateField()
+    enddate=models.DateField()
+
+class Window(models.Model):
+    current_window=models.ForeignKey(Calendar,on_delete=models.CASCADE)
+    current_year=models.IntegerField()
