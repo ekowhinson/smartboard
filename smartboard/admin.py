d8ac7b8e132b1a4f2f75039177811d9711ee0055
Francis Ekow Hinson <ekowhinson@gmail.com>
2022-01-24 00:01:49 +0000

    adminsite

--- smartboard/admin.py
+++ smartboard/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from . import models as smbmodels
 # Register your models here.
+admin.site.register(smbmodels.School)
+admin.site.register(smbmodels.Section)
+admin.site.register(smbmodels.Student)
+admin.site.register(smbmodels.Subject)
