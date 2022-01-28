40489cad3055792ef7191966808c4e6c539e7e5e
Francis Ekow Hinson <ekowhinson@gmail.com>
2022-01-28 08:24:35 +0000

    updates

--- smartboard/models.py
+++ smartboard/models.py
@@ -8,6 +8,13 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class RunningYear(models.Model):
+    academic_year=models.IntegerField()
+    description=models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.code}: {self.name}'
+
 
 class School(models.Model):
     code=models.CharField(max_length=50)
