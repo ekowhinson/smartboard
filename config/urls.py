0e0db0aff6de0ebabc85fb5d498d636beac70f33
ekowhinson <ekowhinson@gmail.com>
2022-01-31 07:56:55 +0000

    nginx working

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 schema_view=get_schema_view(openapi.Info(title="SMARTBOARD API",default_version="v1",description="Smartboard API",terms_of_service="only authorized users",contact=openapi.Contact(email="ekowhinson@gmail.com"),license=openapi.License(name="BSD License"),),public=True,permission_classes=(permissions.AllowAny,),)
 
@@ -31,4 +32,4 @@ urlpatterns = [
     path('swagger/',schema_view.with_ui('swagger',cache_timeout=0),name='schema-swagger-ui'),
     path('redoc/',schema_view.with_ui('redoc',cache_timeout=0),name='schema-redoc'),
     path('api/v1/smartboard/',include('smartboard.urls'))   
-]
+]+ staticfiles_urlpatterns()
